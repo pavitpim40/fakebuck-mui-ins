eb01d3da977b2eb274991615fd774d5ca7ad0a4b
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-08 00:06:39 +0700

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,35 @@
-import { Box } from '@mui/material';
+import { Home, ModeNight, Person, Storefront } from '@mui/icons-material';
+import { Box, List, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material';
 
 export default function Sidebar() {
   return (
-    <Box bgcolor='skyblue' flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List component='div' disablePadding>
+        <ListItemButton sx={{ pl: 4 }} component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary='HomePage' />
+        </ListItemButton>
+        <ListItemButton sx={{ pl: 4 }} component='a' href='#home'>
+          <ListItemIcon>
+            <Storefront />
+          </ListItemIcon>
+          <ListItemText primary='Marketplace' />
+        </ListItemButton>
+        <ListItemButton sx={{ pl: 4 }} component='a' href='#home'>
+          <ListItemIcon>
+            <Person />
+          </ListItemIcon>
+          <ListItemText primary='Friend' />
+        </ListItemButton>
+        <ListItemButton sx={{ pl: 4 }} component='a' href='#home'>
+          <ListItemIcon>
+            <ModeNight />
+            <Switch />
+          </ListItemIcon>
+        </ListItemButton>
+      </List>
     </Box>
   );
 }
