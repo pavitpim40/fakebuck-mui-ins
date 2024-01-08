a35fc38a956f5dd9d1f2da99bd5d4717580576bf
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-09 01:40:01 +0700

    useParams

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function ProfilePage() {
+  const pathObj = useParams();
+  console.log(pathObj);
   return <div>ProfilePage</div>;
 }
 
